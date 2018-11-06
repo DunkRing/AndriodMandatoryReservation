322a14d7fd0e4f4c7045f56bdbd9d26d2ff38b4f
DunkRing <Chris31@live.dk>
2018-11-06 18:47:18 +0100

    fixes
    httpresponse fix
    forkert navne til buttons fixed

--- MandatoryReservation/app/src/main/java/com/example/bruger/mandatoryreservation/AddReservationActivity.java
+++ MandatoryReservation/app/src/main/java/com/example/bruger/mandatoryreservation/AddReservationActivity.java
@@ -16,6 +16,8 @@ import java.io.IOException;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.RequestBody;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class AddReservationActivity extends AppCompatActivity {
     private static final MediaType JSON_MEDIA_TYPE = MediaType.parse("application/json; charset=utf-8");
